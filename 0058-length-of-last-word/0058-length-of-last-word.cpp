02f61a1003078431a9fb01533b8720f33c7755a0
aakashpatil14 <128578064+aakashpatil14@users.noreply.github.com>
2024-04-25 23:01:04 +0530

    Time: 4 ms (46.70%), Space: 7.6 MB (87.11%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans=0;
+        int i=s.size()-1;
+        while(i>=0){
+            if(s[i] != ' '){
+                break;
+            }
+            i--;
+        }
+        while(i>=0){
+            if(s[i]==' '){
+                break;
+            }
+            i--;
+            ans++;
+        }
+        return ans;
+    }
+};
